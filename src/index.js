d6400b38b7842ed86dc0ea796d7fd03e14c2f495
Javier Reyes <jreyesribes@gmail.com>
2019-10-20 12:38:37 +0200

    Challenge createcomment mutation.

--- src/index.js
+++ src/index.js
@@ -93,6 +93,8 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, email: String! age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
   }
 
   type User {
@@ -241,6 +243,51 @@ const resolvers = {
       users.push(user);
 
       return user;
+    },
+    createPost(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author );
+
+      if (!userExists) {
+        throw new Error('User not found!');
+      }
+
+      const post = {
+        id: uuidv4(),
+
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author,
+      };
+
+      posts.push(post);
+
+      return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author );
+
+      if (!userExists) {
+        throw new Error('User not found!');
+      }
+
+      const postExists = users.some((post) => post.id === args.post );
+
+      if (!postExists) {
+        throw new Error('Post not found!');
+      }
+
+      const comment = {
+        id: uuidv4(),
+
+        text: args.text,
+        author: args.author,
+        post: args.post
+      };
+
+      comments.push(comment);
+
+      return comment;
     }
   },
 };
