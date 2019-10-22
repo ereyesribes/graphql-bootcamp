84cc478b9376e80f8b4a342434b6d3539a4f8a69
Javier Reyes <jreyesribes@gmail.com>
2019-10-22 17:08:57 +0200

    deletePost. deleteComment

--- src/index.js
+++ src/index.js
@@ -95,7 +95,9 @@ const typeDefs = `
     createUser(data: CreateUserInput!): User!
     deleteUser(id: ID!): User!
     createPost(data: CreatePostInput!): Post!
+    deletePost(id: ID!): Post!
     createComment(data: CreateCommentInput!): Comment!
+    deleteComment(id: ID!): Comment!
   }
 
   input CreateUserInput {
@@ -302,6 +304,19 @@ const resolvers = {
 
       return post;
     },
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex((post) => post.id == args.id);
+
+      if (postIndex === -1) {
+        throw new Error('Post not found!');
+      }
+
+      const deletedPosts = posts.splice(postIndex, 1);
+
+      comments = comments.filter((comment) => comment.post !== args.id);
+
+      return deletePosts [0];
+    },
     createComment(parent, args, ctx, info) {
       const userExists = users.some((user) => user.id === args.data.author );
 
@@ -323,6 +338,15 @@ const resolvers = {
       comments.push(comment);
 
       return comment;
+    },
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex((comment) => comment.id == args.id);
+
+      if (commentIndex === -1) {
+        throw new Error('Comment not found!');
+      }
+
+      return comments.splice(commentIndex, 1) [0];
     }
   },
 };
