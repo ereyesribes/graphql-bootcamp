b8c7efcfb43d104beb402e5cf0005dc56af9782b
Javier Reyes <jreyesribes@gmail.com>
2019-12-15 16:29:44 +0100

    post subscription events.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -87,27 +87,42 @@ const Mutation = {
 
     if (args.data.published) {
       pubSub.publish('post', {
-        post
+        post: {
+          mutation: 'CREATED',
+          data: post
+        }
       });
     }
 
     return post;
   },
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubSub }, info) {
     const postIndex = db.posts.findIndex((post) => post.id == args.id);
 
     if (postIndex === -1) {
       throw new Error('Post not found!');
     }
 
-    const deletedPosts = db.posts.splice(postIndex, 1);
+    const [post] = db.posts.splice(postIndex, 1);
 
     db.comments = db.comments.filter((comment) => comment.post !== args.id);
 
-    return deletePosts [0];
+    if (post.published) {
+      pubSub.publish('post', {
+        post: {
+          mutation: 'DELETED',
+          data: post
+        }
+      });
+    }
+
+    return post;
   },
-  updatePost(parent, { id, data }, { db }, info) {
-    const post = db.posts.find((post) => post.id === id );
+  updatePost(parent, { id, data }, { db, pubSub }, info) {
+    const
+      post = db.posts.find((post) => post.id === id ),
+      originalPost = { ...post };
+
 
     if (!post) {
       throw new Error('Post not found!');
@@ -124,6 +139,40 @@ const Mutation = {
 
     if (typeof data.published === 'boolean') {
       post.published = data.published;
+
+      if (originalPost.published && !post.published) {
+        // DELETED
+        pubSub.publish('post', {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost
+          }
+        });
+      } else if (!originalPost.published && post.published) {
+        // CREATED
+        pubSub.publish('post', {
+          post: {
+            mutation: 'CREATED',
+            data: post
+          }
+        });
+      } else if (post.published) {
+        // updated
+        pubSub.publish('post', {
+          post: {
+            mutation: 'UPDATED',
+            data: post
+          }
+        });
+      }
+    } else if (post.published) {
+      // updated
+      pubSub.publish('post', {
+        post: {
+          mutation: 'UPDATED',
+          data: post
+        }
+      });
     }
 
     return post;
